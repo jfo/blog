9c0f1084667557cd8199450b505205a16b5f9a88
urthbound <jeffowler@gmail.com>
2015-03-21 19:35:36 -0400

    finishing cleaning up the config file

--- config.rb
+++ config.rb
@@ -1,16 +1,15 @@
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
 set :layout, :standard
+
+activate :syntax
 set :markdown, :fenced_code_blocks => true, :smartypants => true
 set :markdown_engine, :redcarpet
+
+# Middleman provides the Directory Indexes extension to tell Middleman to create 
+# a folder for each .html file and place the built template file as the index 
+# of that folder.
 activate :directory_indexes
-activate :syntax
 
 activate :blog do |blog|
   blog.sources = "posts/:year-:month-:day-:title.html"
   blog.permalink = "/{year}/{month}/{title}.html"
-end
-
-configure :build do
 end
